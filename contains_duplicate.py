22ca00223889a3aa45a70ac622bad41ee0051e48
Jacob Brown <brown.jacob.alexander@gmail.com>
2023-08-15 20:23:05 -0600

    Solution for contains duplicates

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -8,3 +8,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        scene = []
+
+
+        for elem in nums:
+            if elem not in scene:
+                scene.append(elem)
+            else:
+                return True
+        return False
+
+if __name__ == '__main__':
+
+    ins = [[1,2,3,4], [1,2,3,4,1], [1,2,3,4,4], [1,2,3,4,2,5,1,8], [1000, 81, 9999, 432, 0]]
+    expected = [False, True, True, True, False]
+    for i in range(len(ins)):
+        sol = Solution().containsDuplicate(ins[i])
+        print(f'Example {i+1} is {sol==expected[i]} -- Expected {expected[i]} got {sol}')
