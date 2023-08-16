369cfc503cb3d9a6be42f5009eae2652e492aa94
Jacob Brown <brown.jacob.alexander@gmail.com>
2023-08-15 20:08:57 -0600

    Solution to maximum subarray

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def maxSubArray_1(self, nums):
+    def maxSubArray(self, nums):
         """
         This solution looks at every possible subarray and returns the maximum.
         It's super inefficient and there must be a better way. Maybe with pointers?
@@ -8,6 +8,27 @@ class Solution(object):
         :rtype: int
         """
         
+        '''
+        1. Check every subarray
+        2. Record its sum
+        3. If it is the maximum save the subarray
+        '''
+
+        max_subarray_sum = 0
+        num_elements = len(nums)
+        #print(num_elements)
+
+        for subarray_length in range(1, num_elements + 1):
+            for i in range(num_elements - subarray_length + 1):
+                subarray = nums[i:i+subarray_length]
+                #print(i, subarray_length, subarray)
+                subarray_sum = sum(subarray)
+                if subarray_sum > max_subarray_sum:
+                    max_subarray_sum = subarray_sum
+            #print('')
+
+        return max_subarray_sum
+
 
 if __name__ == '__main__':
 
